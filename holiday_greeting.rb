7756abc5da44eb9347a2e63b7c54253e75080349
brodiethiele <brodie.thiele@gmail.com>
2018-07-24 14:57:16 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,3 +1,3 @@
-def holiday_greeting(holiday_one="Mother's Day, Mom",name_two)
+def holiday_greeting(holiday_one="Mother's Day, Mom",name_two=Your Favorite Child")
   "Happy #{holiday_one}! - From #{name_two}"
 end
